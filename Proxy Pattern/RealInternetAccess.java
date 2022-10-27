6430a59feefec6e0a6494acac65fd706d0f5e976
ChamodiePubudini <chamodie1996@gmail.com>
2022-10-27 23:58:03 +0530

    modified

---
+++ Proxy Pattern/RealInternetAccess.java
@@ -0,0 +1,10 @@
+public class RealInternetAccess implements OfficeInternetAccess {  
+    private String employeeName;  
+    public RealInternetAccess(String empName) {  
+        this.employeeName = empName;  
+    }  
+    @Override  
+    public void grantInternetAccess() {  
+        System.out.println("Internet Access granted for employee: "+ employeeName);  
+    }  
+}  
