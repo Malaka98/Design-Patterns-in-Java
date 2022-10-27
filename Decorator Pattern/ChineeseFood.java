818d08387f3421ec2f67ea3ccdf142a8e7eb7f46
nmjayaweera@students.nsbm.lk <nmjayaweera@students.nsbm.ac.lk>
2022-10-28 01:10:27 +0530

    Feat

---
+++ Decorator Pattern/ChineeseFood.java
@@ -0,0 +1,13 @@
+public class ChineeseFood extends FoodDecorator {
+    public ChineeseFood(Food newFood) {
+        super(newFood);
+    }
+
+    public String prepareFood() {
+        return super.prepareFood() + " With Fried Rice and Manchurian  ";
+    }
+
+    public double foodPrice() {
+        return super.foodPrice() + 65.0;
+    }
+}
