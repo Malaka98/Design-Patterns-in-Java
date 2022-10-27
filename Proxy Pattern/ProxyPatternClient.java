012b5b84ed63a72872367b6c3f4b74b397473957
ChamodiePubudini <chamodie1996@gmail.com>
2022-10-27 23:52:49 +0530

    ProxyPatternClient class

---
+++ Proxy Pattern/ProxyPatternClient.java
@@ -0,0 +1,7 @@
+public class ProxyPatternClient {  
+    public static void main(String[] args)   
+    {  
+        OfficeInternetAccess access = new ProxyInternetAccess("Ashwani Rajput");  
+        access.grantInternetAccess();  
+    }  
+}  
