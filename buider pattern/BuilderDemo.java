255cc487b6763e261ba9a4a67c7dcc527053541f
chamika-code <72512887+chamika-code@users.noreply.github.com>
2022-10-29 00:06:57 +0530

    feat

---
+++ buider pattern/BuilderDemo.java
@@ -0,0 +1,10 @@
+public class BuilderDemo {
+    public static void main(String args[]) {
+        CDBuilder cdBuilder = new CDBuilder();
+        CDType cdType1 = cdBuilder.buildSonyCD();
+        cdType1.showItems();
+
+        CDType cdType2 = cdBuilder.buildSamsungCD();
+        cdType2.showItems();
+    }
+}
